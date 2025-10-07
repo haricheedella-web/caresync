c49a12cb4b49a24c865f72ae4b0a264c20647318
haricheedella-web <haricheedella-web@github.com>
2025-10-07 08:03:37 +0530

    Add test content to debug GitHub Pages blank page issue

--- src/App.tsx
+++ src/App.tsx
@@ -10,18 +10,22 @@ import MembershipHealthPlans from './pages/MembershipHealthPlans';
 // GitHub Pages deployment fix
 function App() {
   return (
-    <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services/:serviceId" element={<ServicePage />} />
-          <Route path="/health-insurance-plans" element={<HealthInsurancePlans />} />
-          <Route path="/medicare-consultation" element={<MedicareConsultation />} />
-          <Route path="/membership-plans" element={<MembershipPlans />} />
-          <Route path="/membership-health-plans" element={<MembershipHealthPlans />} />
-        </Routes>
-      </Layout>
-    </Router>
+    <div>
+      <h1>CareSync Test</h1>
+      <p>If you can see this, the app is working!</p>
+      <Router>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services/:serviceId" element={<ServicePage />} />
+            <Route path="/health-insurance-plans" element={<HealthInsurancePlans />} />
+            <Route path="/medicare-consultation" element={<MedicareConsultation />} />
+            <Route path="/membership-plans" element={<MembershipPlans />} />
+            <Route path="/membership-health-plans" element={<MembershipHealthPlans />} />
+          </Routes>
+        </Layout>
+      </Router>
+    </div>
   );
 }
 
